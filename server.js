37bd41018b7fa9dc69c6e98cf74f30653de9fd7e
zakariasan <elitziko@gmail.com>
2024-07-26 15:08:09 +0100

    server plan

--- server.js
+++ server.js
@@ -7,13 +7,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use('/', controllerRouting);
 
-// Global error handler
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(err.status || 500).send({ error: err.message || 'Internal Server Error' });
-});
+controllerRouting(app);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
