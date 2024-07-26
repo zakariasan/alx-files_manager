8556fe78d8000f94692e3560ca1d20b3578ec9ea
zakariasan <elitziko@gmail.com>
2024-07-26 14:47:58 +0100

    utils files

---
+++ utils/basic.js
@@ -0,0 +1,14 @@
+import { ObjectId } from 'mongodb';
+const basicUtils = {
+
+  isValidId(id) {
+    try {
+      ObjectId(id);
+    } catch (err) {
+      return false;
+    }
+    return true;
+  },
+};
+
+export default basicUtils;
